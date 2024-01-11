ea1d0261534ec6b235ca9dfd2eae3e4a57b1380a
mel <malkeet.singh@lendi.com.au>
2024-01-11 15:21:36 +1100

    nextjs auth0 login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css';
-
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div className={styles.container}>
       <Head>
         <title>malkeet.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <img src="https://picsum.photos/200/200" alt="Avatar" className={styles.avatar} />
+      <img
+        src="https://picsum.photos/200/200"
+        alt="Avatar"
+        className={styles.avatar}
+      />
       <div className={styles.title}>Malkeet Singh (Mel)</div>
-      <div className={styles.text}>Software developer - passionate about problem solving and empowering teams.</div>
+      <div className={styles.text}>
+        Software developer - passionate about problem solving and empowering
+        teams.
+      </div>
+      {user ? (
+        <div>
+          Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+          <br></br>
+          Your nickname is {user.name}.
+        </div>
+      ) : (
+        <a href="/api/auth/login">Login</a>
+      )}
     </div>
   );
 }
